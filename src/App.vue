ab4b1cef76a4aeab323cce9ae06509b050c5bfbc
KgMLe <kgodisholebopa@gmail.com>
2023-06-27 16:36:19 +0200

    practice

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar/><!---navigation bar-->
+ <router-view/>  <!-- all the contect shows here -->
+ <Footer/> <!--the footer goes after router view because of the content--->
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default{
+  components:{
+  Navbar, Footer
+  }
+
+}
+
+</script>
 
 <style>
 #app {
@@ -12,19 +23,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Products.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col" v-for="item in data" :key="item.ID"> 
+                    <!-- this is for the key value pairs -->
+                    <div class="class" style="width: 18rem">
+                        <img :src="item.image" class="card-img-top" alt="..">
+                        <div class="card-body">
+                          <h1>{{ item.title }}</h1>
+                          <h4>{{ item.price }}</h4>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                data: []
+            }
+        },
+        methods: {
+        async fetchData(){
+        const  res = await fetch ('https://codebeans2022.github.io/JSON-Server/data.json');
+        let parsedData =  await res.json()
+        this.data = parsedData.products
+            }
+        },
+        mounted (){
+            this.fetchData()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
